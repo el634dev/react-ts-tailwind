caf36ee8119f6b20bface27fb0e79789995cd2fd
el634dev <84924260+el634dev@users.noreply.github.com>
2024-05-30 16:18:08 -0400

    Added boilerplate code

---
+++ react-boilerplate/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex justify-center h-screen items-center bg-gray-200">
+      <h1 className="text-3xl font-bold text-blue-800">
+        Hello World🌍!
+      </h1>
+    </div>
+  );
+}
---
+++ react-boilerplate/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npm install -g npm@10.8.1
